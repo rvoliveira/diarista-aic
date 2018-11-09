9ba73e4ace9e9c5c8a95984ce8c56fa38c5c22ca
rvoliveira.dev <rvoliveira.dev@gmail.com>
2018-11-09 14:57:46 -0200

    versao v0.2 - home com menu

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  pages: Array<{title: string, component: any}>;
+  constructor(public navCtrl: NavController) { }
+  ionViewDidLoad() {
+    this.pages = [
+      { title: 'Agenda', component: 'AgendaPage' },
+      { title: 'Perfil', component: 'PerfilPage' },
+      { title: 'Compras', component: 'ComprasPage' },
+      { title: 'Mapa', component: 'MapaPage' },
+      { title: 'Histórico', component: 'HistoricoPage' },
+      { title: 'Contato', component: 'ContatoPage' },
+      { title: 'Configurações', component: 'ConfiguracoesPage' },
+      { title: 'Sobre', component: 'SobrePage' },
+      { title: 'Sair', component: null },
+    ];
   }
-
+  openPage(page: any) {
+    this.navCtrl.push(page.component);  
+  }
 }
